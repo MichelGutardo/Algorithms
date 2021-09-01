42155f726a619643c53a6d4171d2fa9f82ea6c1d
Michel <michel.i@gmail.com>
2021-08-31 21:01:48 -0300

    Data Structure: Queue using list

---
+++ data_structure/queue_list.py
@@ -0,0 +1,39 @@
+#!/bin/python3
+
+#Queue implementation using list
+#Just use append() to push and remove with pop(0) elements in LIFO order
+
+# List is a Python's built-in data structure, but is quite slow because the shifting all elements by on
+# requiring O(n) 
+
+queue = [] 
+
+
+# append() to push element in the queue
+
+queue.append("M")
+queue.append("i")
+queue.append("c")
+queue.append("h")
+queue.append("e")
+queue.append("l")
+
+
+print("Initial queue")
+print(queue)
+
+# Removing elements 
+print ("\nPop element from queue:")
+print(queue.pop(0))
+print(queue.pop(0))
+print(queue.pop(0))
+print(queue.pop(0))
+print(queue.pop(0))
+print(queue.pop(0))
+
+print ("\nQueue after removing elements:")
+print(queue)
+
+
+
+
